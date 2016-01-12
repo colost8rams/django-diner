5e3e4119e072c3d2ba200c6d5014475bd17778f9
Terry Owen <terry@iknowmac.com>
2016-01-12 08:31:17 -0700

    added unit tests for recipes

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from .models import Recipe, Ingredient, Instruction
 
-# Create your tests here.
+
+class RecipeTestCase(TestCase):
+    
+    def setUp(self):
+        Recipe.objects.create(title='Test recipe')
+        
+    
+    def test_add_delete_ingredient(self):
+        """
+        Test the ability to create a new ingredient and add it to a recipe. Once
+        added, then delete the ingredient.
+        """
+        r = Recipe.objects.get(title='Test recipe')
+        i = r.ingredient_set.create(name='Ingredient 1')
+        self.assertEqual(i.name, 'Ingredient 1')
+        self.assertEqual(r.ingredient_set.count(), 1)
+        i.delete()
+        self.assertEqual(r.ingredient_set.count(), 0)
+        
+    
+    def test_add_instruction(self):
+        """
+        Test the ability to create a new instruction and add it to a recipe. Once
+        added, then delete the instruction.
+        """
+        r = Recipe.objects.get(title='Test recipe')
+        i = Instruction.objects.create(recipe=r,description='Instruction 1')
+        self.assertEqual(i.description, 'Instruction 1')
+        self.assertEqual(r.instruction_set.count(), 1)
+        i.delete()
+        self.assertEqual(r.instruction_set.count(), 0)
+    
